8a025f2a764c19056795112f1e1de9e3235a863f
AlexButeau <alexbuteau1@gmail.com>
2020-12-21 12:27:21 +0100

    -

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const multer = require('multer');
+const fs = require('fs');
+
+const upload = multer({
+  dest: 'tmp/',
+  limits: {
+    fileSize: 3000000,
+  },
+}).array('monfichier');
+
+app.post('/upload', function (req, res, next) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(400).send('File exceeds 3Mo limit');
+    } else if (err) {
+      res.status(400).send('An error occured during upload');
+    }
+
+    req.files.forEach((file) => {
+      fs.rename(file.path, 'public/files/' + file.originalname, (err) => {
+        if (err) console.log(err);
+        console.log('File successfully moved');
+      });
+    });
+    res.status(200).send('Files sent');
+  });
+});
+
+// app.post('/upload', (req, res) => {
+//   upload(req, res, (err) => {
+//     if (err instanceof multer.MulterError) {
+//       res.status(400).send('File exceeds 3Mo limit');
+//     } else if (err) {
+//       res.status(400).send('An error occured during upload');
+//     } else {
+//       req.files.forEach((file) => {
+//         fs.rename(file.path, 'public/files/' + file.originalname, (err) => {
+//           if (err) console.log(err);
+//           console.log('File successfully moved');
+//         });
+//       });
+//       res.status(200).send('Files sent');
+//     }
+//   });
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server listening port ${PORT}`);
+});
